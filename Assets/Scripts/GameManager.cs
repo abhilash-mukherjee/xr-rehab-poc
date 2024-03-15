a9cc71e4b575c03371c18cadf2248288d7cbc468
Abhilash <abhilash301267@gmail.com>
2024-03-16 02:07:02 +0530

    Switching between launcher and gameplay object

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+    }
+}
--- Assets/Scripts/Session Launcher/SessionManager.cs
+++ Assets/Scripts/Session Launcher/SessionManager.cs
@@ -4,6 +4,7 @@ public class SessionManager : MonoBehaviour
 {
     public static SessionManager instance;
     public SessionData currentSessionData;
+    [SerializeField] private GameObject LauncherObject, GamePlayObject;
     private void Start()
     {
         if (instance == null) instance = this;
@@ -19,7 +20,22 @@ public class SessionManager : MonoBehaviour
         }
         var sessionData = SessionDataHelper.GetSessionData(sessionCode);
         currentSessionData = sessionData;
+        LauncherObject.SetActive(false);
+        GamePlayObject.SetActive(true);
         Debug.Log($"Session Launched./n Session Data: [Lang={sessionData.language} Duration={sessionData.durationInSeconds} secs Speed={sessionData.speedOfSession} ]");
     }
-    
+
+    public void KillCurrentSession()
+    {
+        if (string.IsNullOrEmpty(currentSessionData.sessionCode))
+        {
+            Debug.Log($"Session kill failed. No Active session");
+            return;
+        }
+        var debugCode = currentSessionData.sessionCode;
+        currentSessionData = SessionDataHelper.GetEmptySession();
+        LauncherObject.SetActive(true);
+        GamePlayObject.SetActive(false);
+        Debug.Log($"Session Killed. Code = {debugCode}");
+    }
 }
